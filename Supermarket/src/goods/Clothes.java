ce04a83ba4ed6eba771c0c3e6ce1236c110e7f94
dandelion <du2959346375@outlook.com>
2023-05-18 21:52:38 +0800

    Delete Supermarket directory

--- Supermarket/src/goods/Clothes.java
+++
@@ -1,6 +0,0 @@
-package goods;
-public class Clothes extends Goods{
-	   public Clothes(String name, double price, int account) {
-		super(name,price,account);
-	}
-}
--- Supermarket/src/util/OutOfDateException.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class OutOfDateException extends Exception{
-	public OutOfDateException() {};
-	public OutOfDateException(String message) {
-		super(message);
-	}
-
-}
